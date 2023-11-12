e2e824c0d45188517711fb71860dbf8badd4ecc3
Kurosh Zhalerajabi <kjreem.imagine@gmail.com>
2023-11-12 23:03:42 +0330

    [fix]logo:fixed margins

--- src/home/components/logo.jsx
+++ src/home/components/logo.jsx
@@ -6,10 +6,10 @@ import logo from "../../assets/images/IMG_0578-removebg-preview.png";
 const Logo = forwardRef((props, ref) => {
   return (
     <div
-      className="w-[100vw] h-[100vh] bg-[var(--primary-light)] flex flex-col  items-center"
+      className="w-[100vw] h-[100vh] bg-[var(--primary-light)] flex flex-col  items-center gap-14"
       ref={ref} id="home"
     >
-      <div className=" w-full h-5/6 flex flex-col justify-center items-center logo-container">
+      <div className=" w-full h-5/6 flex flex-col justify-center items-center logo-container mt-8">
         <img
           src={logo}
           alt="Logo"
