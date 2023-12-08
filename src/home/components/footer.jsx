a8224a9fc732b6b28c60924b60dbb3cabd9c1603
Kurosh Zhalerajabi <kjreem.imagine@gmail.com>
2023-12-09 01:55:53 +0330

    [fix]footer: fixed auto scroll to footer not aligning with it's bottom

--- src/home/components/footer.jsx
+++ src/home/components/footer.jsx
@@ -9,7 +9,7 @@ function Footer() {
   };
 
   return (
-    <div className="w-[100vw] h-min flex bg-[var(--dark)] p-10 mobile:flex-col mobile:justify-between mobile:items-center mobile:p-0">
+    <div className="w-[100vw] h-min flex bg-[var(--dark)] p-10 mobile:flex-col mobile:justify-between mobile:items-center mobile:p-0 snap-end">
       <div className="h-full w-1/2 flex flex-col gap-10 items-start pl-[10%] mobile:w-full mobile:items-start mobile:gap-5 mobile:pt-10">
         <span
           className="text-[var(--light)] text-[2.5vw] flex mobile:text-[7vw]"
@@ -37,7 +37,7 @@ function Footer() {
           reckontechy
         </span>
       </div>
-      <div className="h-full w-1/2 flex flex-col gap-10 items-center mobile:w-full mobile:items-start mobile:pl-[10%] mobile:gap-5 mobile:mt-5 mobile:mb-3 snap-end">
+      <div className="h-full w-1/2 flex flex-col gap-10 items-center mobile:w-full mobile:items-start mobile:pl-[10%] mobile:gap-5 mobile:mt-5 mobile:mb-3">
         <span
           className="text-[var(--light)] text-[2.5vw] flex mobile:text-[7vw]"
           role="button"
